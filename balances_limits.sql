73b892ce911379c95fb8c34da0702901c122ffbe
golovnev1306 <golovnev1306@mail.ru>
2021-03-24 08:54:40 +0800

    db structure export

--- balances_limits.sql
+++ balances_limits.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Фев 24 2021 г., 10:19
+-- Время создания: Мар 24 2021 г., 03:51
 -- Версия сервера: 10.3.22-MariaDB
 -- Версия PHP: 7.1.33
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- База данных: `balances_limits`
 --
+CREATE DATABASE IF NOT EXISTS `balances_limits` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `balances_limits`;
 
 -- --------------------------------------------------------
 
@@ -31,12 +33,7 @@ CREATE TABLE `bills` (
   `id` int(11) NOT NULL,
   `deal_id` int(11) NOT NULL,
   `number` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `product` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `summ` float NOT NULL,
-  `kvr` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kosgu` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kvfo` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ok` varchar(17) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `summ` double NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -51,14 +48,9 @@ CREATE TABLE `deals` (
   `number` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
   `date` date NOT NULL,
   `product` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `summ` float NOT NULL,
-  `kvr` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kosgu` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kvfo` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ok` varchar(17) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `summ` double NOT NULL,
   `partner` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `balance` float NOT NULL,
-  `limit_id` int(11) NOT NULL
+  `limit_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -74,15 +66,23 @@ CREATE TABLE `limits` (
   `kosgu` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
   `kvfo` varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ok` varchar(17) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `summ` float NOT NULL
+  `summ` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Дамп данных таблицы `limits`
+-- Структура таблицы `payments`
 --
 
-INSERT INTO `limits` (`id`, `name`, `kvr`, `kosgu`, `kvfo`, `ok`, `summ`) VALUES
-(1, 'Увеличение стоимости прочих оборотных запасов (материалов)', '244', '346', '2', '80699900000000000', 8306930);
+CREATE TABLE `payments` (
+  `id` int(11) NOT NULL,
+  `limit_id` int(11) NOT NULL,
+  `number` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `summ` double NOT NULL,
+  `date` date NOT NULL,
+  `purpose_of_payment` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Индексы сохранённых таблиц
@@ -92,19 +92,28 @@ INSERT INTO `limits` (`id`, `name`, `kvr`, `kosgu`, `kvfo`, `ok`, `summ`) VALUES
 -- Индексы таблицы `bills`
 --
 ALTER TABLE `bills`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `deal_id` (`deal_id`);
 
 --
 -- Индексы таблицы `deals`
 --
 ALTER TABLE `deals`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `deals_ibfk_1` (`limit_id`);
 
 --
 -- Индексы таблицы `limits`
 --
 ALTER TABLE `limits`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `limit_id` (`limit_id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -126,7 +135,35 @@ ALTER TABLE `deals`
 -- AUTO_INCREMENT для таблицы `limits`
 --
 ALTER TABLE `limits`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `bills`
+--
+ALTER TABLE `bills`
+  ADD CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`deal_id`) REFERENCES `deals` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `deals`
+--
+ALTER TABLE `deals`
+  ADD CONSTRAINT `deals_ibfk_1` FOREIGN KEY (`limit_id`) REFERENCES `limits` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`limit_id`) REFERENCES `limits` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
